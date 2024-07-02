995eb350a0e219417dfaad686b5cdbed5a089001
Daniel <drgerber69@gmail.com>
2024-07-02 13:30:22 +0200

    Setting up my app

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='flex flex-col'>
+      <section>
+        <header>
 
+
+        </header>
+        <main>
+
+        </main>
+      </section>
+      <h1 className='text-green-400'>Hello</h1>
+      <footer></footer>
     </div>
   )
-    
+
 }
 
 export default App
