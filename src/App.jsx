c8e8d7f9ad5969d9d8c222d6ade9feb842238493
Daniel <drgerber69@gmail.com>
2024-07-02 20:57:29 +0200

    Adaptations made

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
+import HomePage from './Components/HomePage'
+import Header from './Components/Header'
+import FileDisplay from './Components/FileDisplay'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvailable = file || audioStream
+
+  function handleAudioReset() {
+    setFile(null)
+    setAudioStream(null)
+  }
 
   return (
-    <div className='flex flex-col'>
-      <section>
-        <header>
-
-
-        </header>
-        <main>
-
-        </main>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header />
+        {isAudioAvailable ? (
+          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream} />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
       </section>
       <h1 className='text-green-400'>Hello</h1>
       <footer></footer>
