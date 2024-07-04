ff8c876ff5da633468918db78c2f02fd960385ec
Daniel <drgerber69@gmail.com>
2024-07-04 12:15:01 +0200

    Update Transcribing.jsx

--- src/Components/Transcribing.jsx
+++ src/Components/Transcribing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Transcribing() {
     return (
-        <div>Transcribing</div>
+        <div className='flex items-center flex-col justify-center gap-10 md:gap-14 py-24'>
+            <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'><span className='text-red-400 bold'>Transcribing</span></h1>
+        </div>
     )
 }
